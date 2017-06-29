1e43d1d7d882674c1abc5aae8c0349ec936199c2
Simon Redding <s1m0n.r3dd1ng@gmail.com>
2017-06-29 14:57:40 +0100

    Initial empty mix project

---
+++ lib/lend.ex
@@ -0,0 +1,18 @@
+defmodule Lend do
+  @moduledoc """
+  Documentation for Lend.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Lend.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
